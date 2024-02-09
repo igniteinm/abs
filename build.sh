9f38490d734989aebe517c53329606d19d62a39f
igniteinm <111630059+igniteinm@users.noreply.github.com>
2024-02-09 07:54:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,49 +1,26 @@
-#!/bin/bash
-
-set -e
-#Credit to Meghthedev for the initial script 
-
+rm -rf .repo/local_manifests && 
 # Initialize repo with specified manifest
- repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
-
-# Run inside foss.crave.io devspace, in the project folder
-# Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
-# Initialize repo with specified manifest
-# repo init --depth 1 -u https://github.com/sounddrill31/plros_manifests.git -b lineage-20.0 --git-lfs && \
+repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs && 
 # Clone local_manifests repository
-git clone https://github.com/igniteinm/local_manifest --depth 1 -b van .repo/local_manifests && \
+git clone https://github.com/igniteinm/local_manifest --depth 1 -b van .repo/local_manifests && 
 # Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
+
 # Clone Cromite app
-# rm -rf vendor/plros/prebuilt/apps/Cromite;
-# git clone https://gitlab.com/plros-lab/android_packages_apps_Cromite.git vendor/plros/prebuilt/apps/Cromite && \
-# Apply microG patch to frameworks/base
-#cd frameworks/base && \
-#git restore .  && \
-#wget -O microG.patch https://github.com/lineageos4microg/docker-lineage-cicd/raw/master/src/signature_spoofing_patches/android_frameworks_base-Android13.patch && \
-#patch -p1 -i microG.patch && \
-#cd ../.. && \
-# Apply microG patch to packages/modules/Permission
-#cd packages/modules/Permission && \
-#git restore .  && \
-#wget -O microG.patch https://github.com/lineageos4microg/docker-lineage-cicd/raw/master/src/signature_spoofing_patches/packages_modules_Permission-Android13.patch && \
-#patch -p1 -i microG.patch && \
-#cd ../../.. && \
+#rm -rf vendor/plros/prebuilt/apps/Cromite
+
+#git clone https://gitlab.com/plros-lab/android_packages_apps_Cromite.git vendor/plros/prebuilt/apps/Cromite && 
 # Set up build environment
-source build/envsetup.sh && \
+source build/envsetup.sh && 
 # Lunch configuration
-lunch lineage_beryllium-user && \
+lunch lineage_beryllium-user && 
 # Build the ROM
-mka bacon && \
-echo "Date and time:" && \
+mka bacon && echo Date and time: && 
 # Print out/build_date.txt
 cat out/build_date.txt
+ echo 
 # Print SHA256
-sha256sum out/target/product/*/*.zip"
-
-
-
+sha256sum out/target/product/*/*.zip
 
 # Pull generated zip files
 crave pull out/target/product/*/*.zip 
